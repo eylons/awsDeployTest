56417e52f7483b776fe6de62d9632faaccafc76d
eylons <eylon.steiner@gmail.com>
2017-06-30 01:13:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -30,39 +30,9 @@
       
       $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
                       or die("Could not connect: " . mysql_error());
+                      
       print ("Connected successfully");
 
-      // sql to create table
-      $sql = "CREATE TABLE MyGuests (
-      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-      firstname VARCHAR(30) NOT NULL,
-      lastname VARCHAR(30) NOT NULL,
-      email VARCHAR(50),
-      reg_date TIMESTAMP
-      )";
-
-      if ($conn->query($sql) === TRUE) {
-          echo "Table MyGuests created successfully";
-      } else {
-          echo "Error creating table: " . $conn->error;
-      }
-    
-      $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-      VALUES ('John', 'Doe', 'john@example.com')";
-
-      if ($conn->query($sql) === TRUE) {
-          echo "New record created successfully";
-      } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-
-      if ($conn->query($sql) === TRUE) {
-          $last_id = $conn->insert_id;
-          echo "New record created successfully. Last inserted ID is: " . $last_id;
-      } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-    
       $sql = "SELECT id, firstname, lastname FROM MyGuests";
       $result = $conn->query($sql);
 
