9f8a7643ac7a40a88e1b049929fd8e87e3167292
eylons <eylon.steiner@gmail.com>
2017-06-30 13:15:22 -0700

    Create createStackScript.sh

---
+++ createStackScript.sh
@@ -0,0 +1,3 @@
+aws cloudformation create-stack --stackname stackLampAZ
+    --template-body file:///some/local/path/templates/stackTemplateLampAZ.json
+    --parameters file:///some/local/path/params/createStackScriptParams.json
