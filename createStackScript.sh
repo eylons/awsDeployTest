6577bbbf02420c77a2f2f40a34fbb7384ada47ab
eylons <eylon.steiner@gmail.com>
2017-06-30 23:42:04 -0700

    Update createStackScript.sh

--- createStackScript.sh
+++ createStackScript.sh
@@ -1,3 +1,17 @@
-aws cloudformation create-stack --stackname stackLampAZ
-    --template-body file:///some/local/path/templates/stackTemplateLampAZ.json
-    --parameters file:///some/local/path/params/createStackScriptParams.json
+# This script will create a LAMP installation on AWS
+# MZ, RDS
+
+# Make sure you fill your keyName and passwords in the param json file 
+# createStackScriptParams
+
+# Validate template
+aws cloudformation validate-template --template-body file://./stackTemplateLampAZ.json
+
+# Install
+aws cloudformation create-stack --stack-name stackLampAZ --template-body file://./stackTemplateLampAZ.json  --parameters file://./createStackScriptParams.json
+
+# List events example
+aws cloudformation describe-stack-events --stack-name stackLampAZ
+
+# Busy wait till state of stack is complete
+aws cloudformation wait stack-create-complete --stack-name stackLampAZ
